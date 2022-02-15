7ce1be6ed22ba5a999976890ce1e155dc1501c7e
artem.tsarev <artem.tsarev@stm-labs.ru>
2022-02-15 09:32:13 +0300

    Moving to MongoDB

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 console.log('Phonebook server is about to start...')
 
+require('dotenv').config()
+
 const express = require('express')
 const app = express()
 
@@ -15,28 +17,7 @@ morgan.token('body', function (req, res) {
 
 app.use(morgan(':method :url :status :res[content-length] :response-time ms :body'))
 
-let persons = [
-  { 
-    "id": 1,
-    "name": "Arto Hellas", 
-    "number": "040-123456"
-  },
-  { 
-    "id": 2,
-    "name": "Ada Lovelace", 
-    "number": "39-44-5323523"
-  },
-  { 
-    "id": 3,
-    "name": "Dan Abramov", 
-    "number": "12-43-234345"
-  },
-  { 
-    "id": 4,
-    "name": "Mary Poppendieck", 
-    "number": "39-23-6423122"
-  }
-]
+const Person = require('./models/person')
 
 const generateId = () => {
   
@@ -51,18 +32,15 @@ const generateId = () => {
 }
 
 app.get('/api/persons', (request, response) => {
-  response.json(persons)
+  Person.find({}).then(persons => {
+    response.json(persons)
+  })
 })
 
 app.get('/api/persons/:id', (request, response) => {
-
-  const person = persons.find(p => p.id === Number(request.params.id))
-  if (person) {
+  Person.findById(request.params.id).then(person => {
     response.json(person)
-  }
-  else {
-    response.status(404).end()
-  }
+  })
 })
 
 app.post('/api/persons', (request, response) => {
@@ -77,21 +55,15 @@ app.post('/api/persons', (request, response) => {
       error: 'missing number'
     })
   }
-  else if (persons.find(p => p.name === data.name)) {
-    return response.status(400).json({
-      error: 'name must be unique'
-    })
-  }
 
-  const person = {
-    id: generateId(),
+  const person = new Person({
     name: data.name,
-    number: data.number
-  }
+    number: data.number,
+  })
 
-  persons = persons.concat(person)
-
-  response.json(person)
+  person.save().then(savedPerson => {
+    response.json(savedPerson)
+  })
 })
 
 app.delete('/api/persons/:id', (request, response) => {
